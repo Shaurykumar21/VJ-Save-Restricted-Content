b15ef40c70c84ec2511bcd7051486bd541e43e9e
Shaurykumar21 <shaurykumar21@gmail.com>
2025-02-15 22:24:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", "28920407"))
 API_HASH = os.environ.get("API_HASH", "999f6cf8b866224b2741f36b5b44d777")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6073523936"))
+ADMINS = int(os.environ.get("ADMINS", "5859964009"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
